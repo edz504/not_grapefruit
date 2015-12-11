badcb0d43101caa4355a46fb5a302e4a0f48d54f
jessicazou94 <jiaweiz@princeton.edu>
2015-12-11 15:23:09 -0500

    demand update

--- fit_demand/fit_demand_functions_v2.R
+++ fit_demand/fit_demand_functions_v2.R
@@ -6,6 +6,8 @@ library(reshape2)
 cwd <- getwd()
 setwd("/Users/Jessica/not_grapefruit/clean_demand")
 data <- read.csv('MomPopAndNotGrapeFruit_Sales-Price-Region-Product-Year-Month-Source.csv')
+#setwd("/Users/Jessica/not_grapefruit/fit_demand")
+#data1 <- read.csv('MomPop_NotGrapefruit_withIndicator0.csv')
 ora <- subset(data,Product == "ORA")
 poj <- subset(data,Product == "POJ")
 roj <- subset(data,Product == "ROJ")
@@ -30,9 +32,9 @@ for (df in list(ora, poj, roj, fcoj)) {
         model <- lm(fit.df$weekly.demand ~ I(fit.df$price ^ (-2)))
         fit.df$pred <- predict(model)
         ## remove outliers and fit model again
-        fit.df <- subset(fit.df,abs(fit.df$weekly.demand-fit.df$pred)<100)
-        model <- lm(fit.df$weekly.demand ~ I(fit.df$price ^ (-2)))
-        fit.df$pred <- predict(model)
+        #fit.df <- subset(fit.df,abs(fit.df$weekly.demand-fit.df$pred)<100)
+        #model <- lm(fit.df$weekly.demand ~ I(fit.df$price ^ (-2)))
+        #fit.df$pred <- predict(model)
         coef.fit.df[x, ] <- c(model$coef, sum((fit.df$pred - fit.df$weekly.demand)^2)/(length(fit.df$pred)-1),regions[j], products[i])
         #ggplot(fit.df, aes(x=price)) + geom_point(aes(y=weekly.demand)) +
             #geom_line(aes(y=pred), colour='#e74c3c')
@@ -49,5 +51,37 @@ for (df in list(ora, poj, roj, fcoj)) {
 }
 total.SSE
 
-write.csv(coef.fit.df, 'demand_fit_coefs.csv',
+write.csv(coef.fit.df, 'demand_fit_coefs_clean.csv',
           quote=FALSE, row.names=FALSE)
+
+# checking if removing outlier or removing censored demand works better
+# removing outlier seems to work better 
+check = coef.fit.df <- data.frame(matrix(0, 1000, 4))
+total.SSE=0
+tatal.SSE1=0
+setwd("/Users/Jessica/not_grapefruit/fit_demand")
+coef <- read.csv('demand_fit_coefs_outlier.csv')
+setwd("/Users/Jessica/not_grapefruit/fit_demand")
+coef1 <- read.csv('demand_fit_coefs_clean.csv')
+for (product in products) {
+  for (region in regions) {
+    a <- subset(coef$c1, coef$region == region & coef$product == product)
+    b <- subset(coef$intercept, coef$region == region & coef$product == product)
+    price <- subset(data1$Price,coef$region == region & coef$product == product)
+    demand <- subset(data1$Sales,coef$region == region & coef$product == product)/4
+    demand = subset(demand, demand>0)
+    #outlier removed
+    predict <- a/price^2 + b
+    a1 <- subset(coef1$c1, coef$region == region & coef$product == product)
+    b1 <- subset(coef1$intercept, coef$region == region & coef$product == product)
+    #clean (removing all censored)
+    predict1 <- a1/price^2 + b1
+    total.SSE = total.SSE + sum((predict - demand)^2)
+    total.SSE1 = total.SSE + sum((predict1 - demand)^2)
+  }
+}
+total.SSE #removing outliers 165814565
+total.SSE1 #removing censored demand 168710178
+
+
+
